c7f0d71d76757d4e6773bbdaef510c52f01fcc54
ROSH567 <97090887+ROSH567@users.noreply.github.com>
2023-07-23 23:23:57 +0530

    Add files via upload

---
+++ hardhat.config.js
@@ -0,0 +1,48 @@
+
+require("@nomicfoundation/hardhat-toolbox");
+
+const FORK_FUJI = false;
+const FORK_MAINNET = false;
+let forkingData = undefined;
+
+if (FORK_MAINNET) {
+  forkingData = {
+    url: "https://api.avax.network/ext/bc/C/rpcc",
+  };
+}
+if (FORK_FUJI) {
+  forkingData = {
+    url: "https://api.avax-test.network/ext/bc/C/rpc",
+  };
+}
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.18",
+  networks: {
+    hardhat: {
+      gasPrice: 225000000000,
+      chainId: !forkingData ? 43112 : undefined, //Only specify a chainId if we are not forking
+      forking: forkingData
+    },
+    fuji: {
+      url: 'https://api.avax-test.network/ext/bc/C/rpc',
+      gasPrice: 225000000000,
+      chainId: 43113,
+      accounts: [
+       "b1b736d515cad4bb3fd8df036485eb3b99917f05cd242cae74ece234e22235e5"
+      ]
+    },
+    mainnet: {
+      url: 'https://api.avax.network/ext/bc/C/rpc',
+      gasPrice: 225000000000,
+      chainId: 43114,
+      accounts: [
+        // YOUR PRIVATE KEY HERE
+      ]
+    }
+  },
+  etherscan: {
+    apiKey: "RJET8C5G9BCYA8HPSFSI2VXRMIG58PHIRV",
+  },
+}
